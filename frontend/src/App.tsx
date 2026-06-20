f3cccc10874e0fa0772913a24702dc0a774d10db
Allan Silva <allanvss15@gmail.com>
2026-06-20 15:52:38 -0300

    primeira versao

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import EcoTrackPrototype from '../src/components/EcoTrackPrototype'
+
+function App() {
+
+  return (
+    <>
+      <EcoTrackPrototype />
+    </>
+  )
+}
+
+export default App
